1a8d78269343020559710df45913d8381d3da65c
Vladislav Flinski <flinssski@gmail.com>
2025-01-18 14:36:05 +0300

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { readdir, readFile, writeFile } = require('fs/promises');
+
+async function createBundle() {
+  try {
+    const contents = [];
+    const stylesDirPath = path.join(__dirname, 'styles');
+    const files = await readdir(stylesDirPath, { withFileTypes: true });
+
+    for (const file of files) {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        const filePath = path.join(file.path, file.name);
+        const content = await readFile(filePath, { encoding: 'utf-8' });
+        contents.push(content);
+      }
+    }
+
+    const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+    await writeFile(bundleFilePath, contents);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+createBundle();
