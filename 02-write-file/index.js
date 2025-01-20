06f2df58918668c09598d656451eaafa29314e9b
Vladislav Flinski <flinssski@gmail.com>
2025-01-20 16:30:16 +0300

    fix: change program exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,21 +5,25 @@ const { stdin, stdout } = require('process');
 
 const filePath = path.join(__dirname, 'result.txt');
 const writeStream = fs.createWriteStream(filePath);
+const rl = readline.createInterface({ input: stdin, output: stdout });
 
 console.log(
   'Введите текст для записи в файл result.txt\nДля выхода из программы используйте комбинацию клавиш Ctrl + C или введите в консоль команду exit',
 );
 
-const rl = readline.createInterface({ input: stdin, output: stdout });
+function exit() {
+  console.log('Программа завершена. Ваш текст добавлен в файл result.txt');
+  rl.close();
+  writeStream.end();
+  process.exit();
+}
 
 rl.on('line', (input) => {
   if (input.trim() === 'exit') {
-    rl.close();
+    exit();
   } else {
     writeStream.write(`${input}\n`);
   }
 });
 
-rl.on('close', () => {
-  console.log('Программа завершена. Ваш текст добавлен в файл result.txt');
-});
+rl.on('SIGINT', exit);
